f32605e994a7f7d96d1b32679b99204e15598784
refreshingdev <refreshingdev@users.noreply.github.com>
2014-05-16 05:26:09 +0200

    fixed more old package eferences

--- src/com/android/browserlalia/widget/BookmarkThumbnailWidgetProvider.java
+++ src/com/android/browserlalia/widget/BookmarkThumbnailWidgetProvider.java
@@ -33,7 +33,7 @@ import com.android.browserlalia.R;
  */
 public class BookmarkThumbnailWidgetProvider extends AppWidgetProvider {
     public static final String ACTION_BOOKMARK_APPWIDGET_UPDATE =
-        "com.android.browser.BOOKMARK_APPWIDGET_UPDATE";
+        "com.android.browserlalia.BOOKMARK_APPWIDGET_UPDATE";
 
     @Override
     public void onReceive(Context context, Intent intent) {
