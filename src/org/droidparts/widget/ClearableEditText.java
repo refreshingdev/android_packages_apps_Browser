5a87570b5463c24b1f3f5bb71d3698bfdde4b44e
refreshingdev <anonymous>
2014-05-17 11:33:21 +0200

    text box with clear button taken from droidparts lib

---
+++ src/org/droidparts/widget/ClearableEditText.java
@@ -0,0 +1,154 @@
+/**
+ * Copyright 2014 Alex Yanchenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.droidparts.widget;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.view.View.OnTouchListener;
+import android.widget.EditText;
+
+/**
+ * To change clear icon, set
+ *
+ * <pre>
+ * android:drawableRight="@drawable/custom_icon"
+ * </pre>
+ */
+public class ClearableEditText extends EditText implements OnTouchListener,
+        OnFocusChangeListener {
+
+    public interface Listener {
+        void didClearText();
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    private Drawable xD;
+    private Listener listener;
+
+    public ClearableEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    @Override
+    public void setOnTouchListener(OnTouchListener l) {
+        this.l = l;
+    }
+
+    @Override
+    public void setOnFocusChangeListener(OnFocusChangeListener f) {
+        this.f = f;
+    }
+
+    private OnTouchListener l;
+    private OnFocusChangeListener f;
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (getCompoundDrawables()[2] != null) {
+            boolean tappedX = event.getX() > (getWidth() - getPaddingRight() - xD
+                    .getIntrinsicWidth());
+            if (tappedX) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    setText("");
+                    if (listener != null) {
+                        listener.didClearText();
+                    }
+                }
+                return true;
+            }
+        }
+        if (l != null) {
+            return l.onTouch(v, event);
+        }
+        return false;
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (hasFocus) {
+            setClearIconVisible(isNotEmpty(getText()));
+        } else {
+            setClearIconVisible(false);
+        }
+        if (f != null) {
+            f.onFocusChange(v, hasFocus);
+        }
+    }
+
+    public void onTextChanged(EditText view, String text) {
+        if (isFocused()) {
+            setClearIconVisible(isNotEmpty(text));
+        }
+    }
+
+    private boolean isNotEmpty(CharSequence text) {
+        return text != null && text.length() > 0;
+    }
+
+    private void init() {
+        xD = getCompoundDrawables()[2];
+        if (xD == null) {
+            xD = getResources()
+                    .getDrawable(android.R.drawable.presence_offline);
+        }
+        xD.setBounds(0, 0, xD.getIntrinsicWidth(), xD.getIntrinsicHeight());
+        setClearIconVisible(false);
+        super.setOnTouchListener(this);
+        super.setOnFocusChangeListener(this);
+        addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ClearableEditText.this.onTextChanged(ClearableEditText.this, s.toString());
+            }
+        });
+    }
+
+    protected void setClearIconVisible(boolean visible) {
+        Drawable x = visible ? xD : null;
+        setCompoundDrawables(getCompoundDrawables()[0],
+                getCompoundDrawables()[1], x, getCompoundDrawables()[3]);
+    }
+}
